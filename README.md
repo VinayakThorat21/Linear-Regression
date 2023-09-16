5f103b98ec03c2fe7c3ece315a84d337eb520700
Vinayak M. Thorat <thorat.vinayak.0510@gmail.com>
2023-09-16 17:31:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Linear-Regression
+ Analysed impact of LSD via Average Math Test score using Linear Regression Algorithm from Sci-Kit Learn
---
+++ variables.py
@@ -0,0 +1,11 @@
+# this python file is meant to demostrate import of modules and access to objects under it
+import math
+
+def sq_root(number):
+    return math.sqrt(number)
+
+var_1 = 1
+var_2 = 21
+var_3 = 3.14
+var_4 = 1.71
+var_5 = 'string'
